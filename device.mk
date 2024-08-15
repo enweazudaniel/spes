3250a941859a642f820baac182944887e77abb90
enweazudaniel <daniel.enweazu@student.aul.edu.ng>
2024-08-15 13:12:23 +0000

    superiorify

--- device.mk
+++ device.mk
@@ -209,7 +209,12 @@ PRODUCT_PACKAGES += \
     libsdmcore \
     libsdmutils \
     libtinyxml \
-    libvulkan
+    libvulkan \
+    libjsoncpp.vendor \
+    libsqlite.vendor \
+    libprocessgroup.vendor \
+    libcurl \
+    libpng.vendor
 
 PRODUCT_PACKAGES += \
     disable_configstore
